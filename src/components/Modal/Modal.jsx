cb3500371ae51d90954d1067f443193730f18ea1
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-02-28 23:52:57 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,17 +3,17 @@ import { useEffect } from 'react';
 import style from './Modal.module.css';
 
 export const Modal = ({ largeImageURL, toggleModal }) => {
-  const handleEscClick = event => {
-    if (event.key !== 'Escape') return;
-
-    toggleModal();
-  };
-
   useEffect(() => {
+    const handleEscClick = event => {
+      if (event.key !== 'Escape') return;
+
+      toggleModal();
+    };
+
     document.addEventListener('keydown', handleEscClick);
 
     return () => document.removeEventListener('keydown', handleEscClick);
-  }, [handleEscClick]);
+  }, [toggleModal]);
 
   const handleClick = event => {
     if (event.target === event.currentTarget) {
